1669de9ecb4adecdc0fc7317e609f280c0a083c1
DenisKrasheninnikov <krasheninnikov.0792@gmail.com>
2022-03-20 16:24:30 +0300

    правки в requirements

--- conf/settings.py
+++ conf/settings.py
@@ -124,6 +124,7 @@ STATIC_URL = '/static/'
 STATICFILS_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
